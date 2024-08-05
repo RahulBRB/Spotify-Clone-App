b47670ef9679f5f100bd5084289fbbca02a16b35
R A H U L <86495244+RahulBRB@users.noreply.github.com>
2024-08-05 19:05:16 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,31 +3,21 @@ import styled from "styled-components";
 
 export default function Login() {
   const handleClick = async () => {
-    const client_id = "844876e388a04954bac8fba5ed951770";
-    const redirect_uri = "https://my-spotify-app-olive.vercel.app/";
-    const api_uri = "https://accounts.spotify.com/authorize";
-    const scope = [
-      "user-read-private",
-      "user-read-email",
-      "user-modify-playback-state",
-      "user-read-playback-state",
-      "user-read-currently-playing",
-      "user-read-recently-played",
-      "user-top-read",
-    ];
-    window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope.join(
-      " "
-    )}&response_type=token&show_dialog=true`;
-  };
-  return (
-    <Container>
-      <img
-        src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Black.png"
-        alt="spotify"
-      />
-      <button onClick={handleClick}>Connect Spotify</button>
-    </Container>
-  );
+  const client_id = "844876e388a04954bac8fba5ed951770";
+  const redirect_uri = encodeURIComponent("https://spotify-clone-rb.vercel.app/");
+  const api_uri = "https://accounts.spotify.com/authorize";
+  const scope = [
+    "user-read-private",
+    "user-read-email",
+    "user-modify-playback-state",
+    "user-read-playback-state",
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-top-read",
+  ];
+  const scopeString = encodeURIComponent(scope.join(" "));
+  window.location.href = `${api_uri}?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scopeString}&response_type=token&show_dialog=true`;
+};
 }
 
 const Container = styled.div`
